ef7520827245d3f577ab90c9d6b28cfe10476542
kevyn768 <kevyn.nasimento@aluno.ce.gov.br>
2024-08-22 15:39:19 -0300

    adicionarlivro

--- database-memory.js
+++ database-memory.js
@@ -3,5 +3,9 @@ import { truncateSync } from "node:fs"
 
 export class DatabaseMemory {
     #livros = new Map()
+    create(livro) {
+        const livroID = randomUUID()
 
+        this.#livros.set(livroID,livro)
+    }
 }
--- server.js
+++ server.js
@@ -4,5 +4,19 @@ import { DatabaseMemory } from "./database-memory.js";
 const server = fastify()
 const database = new DatabaseMemory()
 
+server.post('/livros', (request, reply) => {
+    const { titulo, autor, ano, lido, nota } = request.body
+
+    database.create({
+        titulo,
+        autor,
+        ano,
+        nota,
+        lido
+    })
+
+
+    return reply.status(204).send()
+})
 
 server.listen({ port: 3333 })
